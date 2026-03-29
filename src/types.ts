22bd9f57b9049d5248d36689797eadd8f2e3fc70
Khalidfarah1 <khalidfarah9@gmail.com>
2026-03-29 15:45:51 +0100

    feat: add Spotify API types

---
+++ src/types.ts
@@ -0,0 +1,46 @@
+export type TimeRange = 'short_term' | 'medium_term' | 'long_term'
+
+export interface SpotifyImage {
+  url: string
+  width: number | null
+  height: number | null
+}
+
+export interface SpotifyUser {
+  id: string
+  display_name: string
+  images: SpotifyImage[]
+}
+
+export interface SpotifyArtist {
+  id: string
+  name: string
+  images: SpotifyImage[]
+}
+
+export interface SpotifyTrack {
+  id: string
+  name: string
+  artists: SpotifyArtist[]
+  album: {
+    name: string
+    images: SpotifyImage[]
+  }
+}
+
+export interface SpotifyRecentlyPlayedItem {
+  track: SpotifyTrack
+  played_at: string
+}
+
+export interface TopTracksResponse {
+  items: SpotifyTrack[]
+}
+
+export interface TopArtistsResponse {
+  items: SpotifyArtist[]
+}
+
+export interface RecentlyPlayedResponse {
+  items: SpotifyRecentlyPlayedItem[]
+}
